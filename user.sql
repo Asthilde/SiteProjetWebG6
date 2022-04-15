52d48be22591972a0a2277e12c30d4cdb7a87253
Melipatamaa <pyrollineisafirefox@gmail.com>
2022-04-15 15:43:47 +0200

    compte utilisateur

---
+++ user.sql
@@ -0,0 +1,5 @@
+# Privilèges pour `noemine`@`localhost:`
+
+GRANT ALL PRIVILEGES ON *.* TO `noemine`@`localhost:` IDENTIFIED BY PASSWORD '*4E27335FEA9D9E39AD755E0D2B062A05AE15D7F6' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON `projet_histoire_arjo_pellegrin`.* TO `noemine`@`localhost:`;
