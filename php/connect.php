0c88aea0a0c0e7edd23f6701adcec0f7ea25ba07
Asthilde <noemie.pellegrin@live.fr>
2022-04-15 15:52:30 +0200

    Ajout dossiers parents

---
+++ php/connect.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost:";
+$dbname = "projet_histoire_arjo_pellegrin";
+$username = "noemine";
+$password = "patioPalmierChat200522";
+
+try { 
+    $BDD = new PDO( "mysql:host=". $host .";dbname=". $dbname .";charset=utf8",
+        $username,
+        $password, 
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e) {
+    die('Erreur fatale : ' . $e->getMessage());
+}
+?>
--- sql/database.sql
+++ sql/database.sql
@@ -1,4 +1,5 @@
-# Privilèges pour `noemine`@`localhost:`
+create database if not exists projet_histoire_arjo_pellegrin character set utf8 collate utf8_unicode_ci;
+use projet_histoire_arjo_pellegrin;
 
 GRANT ALL PRIVILEGES ON *.* TO `noemine`@`localhost:` IDENTIFIED BY PASSWORD '*4E27335FEA9D9E39AD755E0D2B062A05AE15D7F6' WITH GRANT OPTION;
 
