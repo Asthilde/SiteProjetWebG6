91fec5acdfa1ecd6d308be6f1c7d527e3c0adf9e
Asthilde <noemie.pellegrin@live.fr>
2022-04-25 17:31:06 +0200

    Test pour ajout_hist qui fonctionne pas

---
+++ php/functions.php
@@ -0,0 +1,34 @@
+<?php 
+function pages ($tab, $racine, $niv, $num_actuel) {
+    if($racine.$niv.$num_actuel == "A3B3C3D3")
+        return $tab;
+    if($niv == "A"){
+        array_push($tab, "A".$num_actuel);
+    }
+    else{
+        array_push($tab, $racine.$niv.$num_actuel);
+    }
+    if($num_actuel == 3){
+        if($niv == "A"){
+            pages($tab, "A1", 'B', 1);
+        }
+        else if(substr($racine, -1) == '3'){ //Cas où on atteint le dernier choix du niveau parent
+            pages($tab, $racine.$niv.'1', chr((ord($niv)+1)), 1);
+        }
+        else{ //Cas où on atteint le dernier choix du niveau actuel mais pas parent
+            pages($tab, substr($racine,0,strlen($racine-1)).$niv.(string)(substr($racine,-1) - '-1'), $niv, 1);
+        }
+    }
+    else{
+        if($niv == "A"){
+            pages($tab, null, $niv, $num_actuel+1);
+        }
+        else{
+            pages($tab, $racine, $niv, $num_actuel+1);
+        }
+    }
+};
+//echo (string)(substr("1",-1) - '-1');
+$tab=null;
+echo var_dump(pages($tab, null, 'A', 1));
+?>
