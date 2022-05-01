869055286096e10a6fea70472cc50454bc5dea96
Asthilde <noemie.pellegrin@live.fr>
2022-05-01 22:48:16 +0200

    Essai de faire fonctionner ajout_page, probleme pour affichage si choix de fin plus tot

--- php/ajout_page.php
+++ php/ajout_page.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 require_once("connect.php");
-require("functions.php");
-if (!isset($_POST['pageChoisie'])) { //$_SESSION['num_page'] == 0) {
+//require("functions.php");
+if ($_SESSION['num_page'] == 0) {
   $req2 = "SELECT * FROM histoire WHERE nom_hist = '{$_SESSION['nom_hist']}'";
   $res2 = $BDD->query($req2);
   $idHist = $res2->fetch();
   $_SESSION['id_hist'] = $idHist['id_hist'];
 }
 if (isset($_POST['para_1']) || isset($_POST['img_1'])) {
-  $req = $BDD->prepare("SELECT COUNT(*) as nb FROM page_hist WHERE id_page='{$_POST['pageChoisie']}'");
+  $req = $BDD->prepare("SELECT COUNT(*) as nb FROM page_hist WHERE id_page='{$_POST['pageChoisie']}' AND id_hist = '{$_SESSION['id_hist']}'");
   $req->execute();
   /*$req->execute(array(
     "idPage" => $_POST['pageChoisie']
@@ -42,7 +42,7 @@ if (isset($_POST['para_1']) || isset($_POST['img_1'])) {
     for($i=1; $i < 6; $i++){
       $nom = "img_" . $cpt;
       if(isset($_FILES[$nom])) { //Voir comment gérer le nom
-        $_FILES[$nom]['name'] =  strtolower("image_{$_POST['pageChoisie']}_{$cpt}" . substr($_FILES[$nom]['name'], strpos($_FILES[$nom]['name'], '.')));
+        $_FILES[$nom]['name'] =  strtolower("img_{$_SESSION['id_hist']}_{$_POST['pageChoisie']}_{$cpt}" . substr($_FILES[$nom]['name'], strpos($_FILES[$nom]['name'], '.')));
         if (move_uploaded_file($_FILES[$nom]['tmp_name'], "../images/" . $_SESSION['nom_hist'] . $_FILES[$nom]['name'])) {
           $tab[$nom] = $_FILES[$nom]['name'];  
         }
@@ -58,7 +58,7 @@ if (isset($_POST['para_1']) || isset($_POST['img_1'])) {
 
     for($i=1; $i < 4; $i++){
       $nom = "choix" . $cpt; 
-      if($_POST['pageChoisie'] == 0){
+      if($_POST['pageChoisie'] == '0'){
         $nivSuiv = 'A';
         $nomPageCible = 'A' . $i;
       }
@@ -69,7 +69,7 @@ if (isset($_POST['para_1']) || isset($_POST['img_1'])) {
         //echo ' - PageCible : ' . $nomPageCible . '<br/>';
       }
       $sql = "INSERT INTO choix (id_page, id_page_cible, id_hist, contenu) VALUES (:numPage, :numPageCible, :numHist, :choix)"; //'{$_SESSION['num_page']}','{$_SESSION['id_hist']}', :para'{$cpt}')";
-      $req = $BDD->prepare($sql);//Problème au niveau du num_page car il n'y a que des int
+      $req = $BDD->prepare($sql);
       $req->execute(array(
         'numPage' => $_POST['pageChoisie'],
         'numPageCible' => $nomPageCible,
@@ -85,7 +85,6 @@ if (isset($_POST['para_1']) || isset($_POST['img_1'])) {
     <a href="index.php">RETOUR</a>
     <a href="fin_hist.php"> TERMINER L'HISTOIRE</a>
 <?php
- //var_dump($ligne['nb']);
   }
 }
 ?>
@@ -124,29 +123,50 @@ if (isset($_POST['para_1']) || isset($_POST['img_1'])) {
           <label class="col-sm-4 control-label">Page à remplir</label>
           <div class="col-sm-6">
             <select class="form-control" id="pageChoisie" name="pageChoisie" required>
-                <?php if(!isset($_POST['pageChoisie'])){ ?>
+                <?php if(count($tabPages) == 0){ ?>
                   <option value="0">0</option>
                 <?php }
-                $tab = array();
-                pages($tab, null, 'A', 1);
-                /*$pagesImpossible = array();
-                if(isset($_POST['para_1']) || isset($_POST['img_1'])){
-                  $req = $BDD->prepare("SELECT * FROM page_hist WHERE id_hist=:numero");
+                $pagesARenseigner = array("A1", "A2", "A3", 
+                "A1B1", "A1B2", "A1B3", "A2B1", "A2B2", "A2B3", "A3B1", "A3B2", "A3B3", 
+                "A1B1C1", "A1B1C2", "A1B1C3", "A1B2C1", "A1B2C2", "A1B2C3", "A1B3C1", "A1B3C2", "A1B3C3", 
+                "A2B1C1", "A2B1C2", "A2B1C3", "A2B2C1", "A2B2C2", "A2B2C3", "A2B3C1", "A2B3C2", "A2B3C3",
+                "A3B1C1", "A3B1C2", "A3B1C3", "A3B2C1", "A3B2C2", "A3B2C3", "A3B3C1", "A3B3C2", "A3B3C3",
+                "A1B1C1D1", "A1B1C1D2", "A1B1C1D3", "A1B1C2D1", "A1B1C2D2", "A1B1C2D3", "A1B1C3D1", "A1B1C3D2", "A1B1C3D3",
+                "A1B2C1D1", "A1B2C1D2", "A1B2C1D3", "A1B2C2D1", "A1B2C2D2", "A1B2C2D3", "A1B2C3D1", "A1B2C3D2", "A1B2C3D3",
+                "A1B3C1D1", "A1B3C1D2", "A1B3C1D3", "A1B3C2D1", "A1B3C2D2", "A1B3C2D3", "A1B3C3D1", "A1B3C3D2", "A1B3C3D3",
+                "A2B1C1D1", "A2B1C1D2", "A2B1C1D3", "A2B1C2D1", "A2B1C2D2", "A2B1C2D3", "A2B1C3D1", "A2B1C3D2", "A2B1C3D3",
+                "A2B2C1D1", "A2B2C1D2", "A2B2C1D3", "A2B2C2D1", "A2B2C2D2", "A2B2C2D3", "A2B2C3D1", "A2B2C3D2", "A2B2C3D3",
+                "A3B3C1D1", "A3B3C1D2", "A3B3C1D3", "A3B3C2D1", "A3B3C2D2", "A3B3C2D3", "A3B3C3D1", "A3B3C3D2", "A3B3C3D3",
+                );
+                //pages($tab, null, 'A', 1);
+                if(count($tabPages) > 0){
+                  $req = $BDD->prepare("SELECT * FROM choix WHERE id_hist=:numero");
                   $req->execute(array(
                     "numero" => $_SESSION['id_hist']
-                  )); CONTINUER CA*/
-                foreach($tab as $page){ 
-                  $dejaRenseigne = false;
-                  foreach($tabPages as $pageRenseignee){
-                    if($page == $pageRenseignee){ 
-                      $dejaRenseigne = true;
-                      break;
-                    }
+                  )); //CONTINUER CA
+                  while($ligne = $req->fetch()){
+                    if($ligne['contenu'] == "FIN"){
+                      for($i = 0; $i < count($pagesARenseigner); $i++){ 
+                        if($pagesARenseigner['{$i}'] == $ligne['id_page_cible'] || (str_contains($tab['{$i}'], $ligne['id_page_cible']) && strlen($tab['{$i}']) >= strlen($ligne['id_page_cible']))){
+                          unset($pagesARenseigner['{$i}']);
+                        }
+                      }
+                    }//A revoir quand il faut que l'utilisateur ne puisse pas rentrer des choix du niv n+2
                   }
-                  if(!$dejaRenseigne) {?>
-                  <option value="<?= $page ?>"><?= $page ?></option>
-                  <?php }
-                } ?>
+                }
+                  foreach($pagesARenseigner as $page){ 
+                    $dejaRenseigne = false;
+                    foreach($tabPages as $pageRenseignee){
+                      if($page == $pageRenseignee){ 
+                        $dejaRenseigne = true;
+                        break;
+                      }
+                    }
+                    if(!$dejaRenseigne) {?>
+                    <option value="<?= $page ?>"><?= $page ?></option>
+                    <?php }
+                  }
+                  var_dump($pagesARenseigner);?>
              </select>
             </div>
         </div>
@@ -166,7 +186,7 @@ if (isset($_POST['para_1']) || isset($_POST['img_1'])) {
         <?php } ?>
         <?php for ($i = 1; $i < 4; $i++) { ?>
           <div class="form-group">
-            <label class="col-sm-4 control-label">Choix <?= $i ?> (si c'est la fin de la branche écrire NULL dans l'encadré)</label>
+            <label class="col-sm-4 control-label">Choix <?= $i ?> (si c'est la fin de la branche écrire FIN dans l'encadré)</label>
             <div class="col-sm-6">
               <input type="text" name="choix<?= $i ?>" value="" class="form-control" placeholder="Ecrivez le choix<?= $i ?>" <?php if ($i == 1) { ?>required <?php } ?> autofocus>
             </div>
--- php/functions.php
+++ php/functions.php
@@ -14,23 +14,91 @@ function pages (&$tab, $racine, $niv, $num_actuel) {
         array_push($tab, $racine.$niv.$num_actuel);
     }
     if($num_actuel == 3){
-        if($niv == "A"){
+        if($niv == "A"){ //Cas A3
             pages($tab, "A1", 'B', 1);
         }
+        else if($niv == "B"){
+            if(substr($racine, -1) == '3'){ //Cas A3B3
+                pages($tab, "A1B1", 'C', 1);
+            }
+            else //Cas A1B3
+                pages($tab, "A".((string)(substr($racine,-1) - '-1')), 'B', 1);
+        }
+        else if($niv =="C"){
+            if(substr($racine, -1) == '3'){ 
+                if(substr($racine, 1,1) == '3') //Cas A3B3C3
+                    pages($tab, "A1B1C1", 'D', 1);
+                else //Cas A1B3C3
+                    pages($tab, "A".((string)(substr($racine,1,1) - '-1')) . 'B1', 'C', 1);
+            }
+            else { //Cas A1B1C3
+                pages($tab, substr($racine, 0, 3) . ((string)(substr($racine,-1) - '-1')), 'C', 1);
+            }
+        }
+        else if($niv =="D"){
+            if(substr($racine, -1) == '3'){ //C3
+                if(substr($racine, -3, 1) == '3')// B3C3
+                    pages($tab, substr($racine, 0, 1) . ((string)(substr($racine,1,1) - '-1')) . 'B1C1', 'D', 1);
+                else //Cas A3B1C3D3
+                    pages($tab, substr($racine, 0, 3) . ((string)(substr($racine,3,1) - '-1')) . 'C1', 'D', 1);
+            }
+            else{
+                if(substr($racine, -3, 1) == '3'){
+                    if(substr($racine, 1,1) == '3') //Cas A3B3C1D3
+                        pages($tab, 'A1B1C' . ((string)(substr($racine,-1) - '-1')), 'D', 1);
+                }//Cas A1B3C1D3
+                    pages($tab, substr($racine, 0, 1) . ((string)(substr($racine,1,1) - '-1')) . 'B1C1', 'D', 1);
+                
+            }
+                }//Cas A3B3C3
+                    /*pages($tab, "A1B1C1", 'D', 1);
+                else //Cas A1B3C3
+                    pages($tab, "A".((string)(substr($racine,1,1) - '-1')) . 'B1', 'C', 1);
+            }*/
+            else { //Cas A1B1C3
+                pages($tab, substr($racine, 0, 3) . ((string)(substr($racine,-1) - '-1')), 'C', 1);
+            }
+        }
         else if(substr($racine, -1) == '3'){ //Cas où on atteint le dernier choix du niveau parent
-            pages($tab, $racine.$niv.'1', chr((ord($niv)+1)), 1);
+            if(count($racine) == 2)
+                pages($tab, $racine.$niv.'1', chr((ord($niv)+1)), 1);
         }
         else{ //Cas où on atteint le dernier choix du niveau actuel mais pas parent
             pages($tab, substr($racine,0,strlen($racine)-1).((string)(substr($racine,-1) - '-1')), $niv, 1);
         }
     }
-    else{
-        if($niv == "A"){
-            pages($tab, null, $niv, $num_actuel+1);
-        }
-        else{
-            pages($tab, $racine, $niv, $num_actuel+1);
-        }
+function parentDepart($racine, $nivInter, $nivActu){
+    if(count($racine) == 2)
+        return substr($racine,-1).'1'.$nivInter.$nivActu.'1';
+    else if(count($racine) == 2){
+        return substr($racine,-1).((string)(substr($racine,-1) - '-1')).$nivInter.$nivActu.'1';
     }
-};
-
+    else if(substr($racine, -1) == '3'){
+        parentDepart(substr($racine,0, strlen($racine)-2),substr($racine, -2,1).'1'.$nivInter, $nivActu);
+    }
+    else{
+        parentDepart(substr($racine,0, strlen($racine)-2),substr($racine, -2,1).'1'.$nivInter, $nivActu);
+    }
+}
+/*$tab = array();
+pages($tab, '', 'A', 1);*/
+$pagesARenseigner = array("A1", "A2", "A3", 
+                "A1B1", "A1B2", "A1B3", "A2B1", "A2B2", "A2B3", "A3B1", "A3B2", "A3B3", 
+                "A1B1C1", "A1B1C2", "A1B1C3", "A1B2C1", "A1B2C2", "A1B2C3", "A1B3C1", "A1B3C2", "A1B3C3", 
+                "A2B1C1", "A2B1C2", "A2B1C3", "A2B2C1", "A2B2C2", "A2B2C3", "A2B3C1", "A2B3C2", "A2B3C3",
+                "A3B1C1", "A3B1C2", "A3B1C3", "A3B2C1", "A3B2C2", "A3B2C3", "A3B3C1", "A3B3C2", "A3B3C3",
+                "A1B1C1D1", "A1B1C1D2", "A1B1C1D3", "A1B1C2D1", "A1B1C2D2", "A1B1C2D3", "A1B1C3D1", "A1B1C3D2", "A1B1C3D3",
+                "A1B2C1D1", "A1B2C1D2", "A1B2C1D3", "A1B2C2D1", "A1B2C2D2", "A1B2C2D3", "A1B2C3D1", "A1B2C3D2", "A1B2C3D3",
+                "A1B3C1D1", "A1B3C1D2", "A1B3C1D3", "A1B3C2D1", "A1B3C2D2", "A1B3C2D3", "A1B3C3D1", "A1B3C3D2", "A1B3C3D3",
+                "A2B1C1D1", "A2B1C1D2", "A2B1C1D3", "A2B1C2D1", "A2B1C2D2", "A2B1C2D3", "A2B1C3D1", "A2B1C3D2", "A2B1C3D3",
+                "A2B2C1D1", "A2B2C1D2", "A2B2C1D3", "A2B2C2D1", "A2B2C2D2", "A2B2C2D3", "A2B2C3D1", "A2B2C3D2", "A2B2C3D3",
+                "A3B3C1D1", "A3B3C1D2", "A3B3C1D3", "A3B3C2D1", "A3B3C2D2", "A3B3C2D3", "A3B3C3D1", "A3B3C3D2", "A3B3C3D3",
+                );
+for($i = 0; $i < count($pagesARenseigner); $i++){ 
+    echo count($pagesARenseigner);
+    var_dump($pagesARenseigner[$i]); //Le str_contains ne fonctionne pas je ne sais pas pourquoi ...
+    if($pagesARenseigner[$i] == 'A1' || (str_contains($pagesARenseigner[$i], 'A1') && strlen($pagesARenseigner[$i]) >= strlen('A1'))){
+      unset($pagesARenseigner[$i]);
+    }
+  }
+  var_dump($pagesARenseigner);
