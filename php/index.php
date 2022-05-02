3e8763df22b1570aedaf5d6a03f226256bef1aeb
Melipatamaa <pyrollineisafirefox@gmail.com>
2022-05-02 16:13:25 +0200

    pb avec l'affichage des choix, épisode 1000

--- php/index.php
+++ php/index.php
@@ -11,7 +11,7 @@ require_once 'connect.php' ?>
     include 'templatesHTML/navbar.php';
     $demarrage = "connexion.php";
     if (isset($_SESSION['login'])) {
-      $demarrage = "jeu.php";
+      $demarrage = "jeu.php?id=";
     }
 
     if ($BDD) {
@@ -22,46 +22,46 @@ require_once 'connect.php' ?>
         $res2 = $BDD->query($req2);
         $maLigne = $res2->fetch();
     ?>
-    <article>
-      <!--Il faut vérifier ici si l'utilisateur est connecté -->
-      <div class="row align-items-end">
-        <h3 class="col"><?php echo $ligne['nom_hist']; ?></h3>
-        <div class="col" id="nomCreateur"> <?= $maLigne['pseudo']; ?> </div>
-        <div class="form-group">
-          <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-            <a class="btn btn-default btn-primary" href=<?= $demarrage ?>> Démarrer </a>
-          </div>
-
-          <?php
-          if (isset($_SESSION["admin"])) {
-          ?>
-            <div class="col" id="imgModifSuppr">
-              <a class="bi bi-pencil-square" href="modifier.php?id=<?= $ligne['id_hist'] ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                </svg></a>
-              <a class="bi bi-trash3-fill" href="supprimer.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
-                </svg></a>
+        <article>
+          <!--Il faut vérifier ici si l'utilisateur est connecté -->
+          <div class="row align-items-end">
+            <h3 class="col"><?php echo $ligne['nom_hist']; ?></h3>
+            <div class="col" id="nomCreateur"> <?= $maLigne['pseudo']; ?> </div>
+            <div class="form-group">
               <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-                <a class="btn btn-default btn-primary" href="histoires.php?id=<?= $ligne['id_hist'] ?>"> Plus d'infos </a>
+                <a class="btn btn-default btn-primary" href=<?= $demarrage . $ligne['id_hist'] ?>> Démarrer </a>
               </div>
+
+              <?php
+              if (isset($_SESSION["admin"])) {
+              ?>
+                <div class="col" id="imgModifSuppr">
+                  <a class="bi bi-pencil-square" href="modifier.php?id=<?= $ligne['id_hist'] ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                    </svg></a>
+                  <a class="bi bi-trash3-fill" href="supprimer.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                      <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                    </svg></a>
+                  <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                    <a class="btn btn-default btn-primary" href="histoires.php?id=<?= $ligne['id_hist'] ?>"> Plus d'infos </a>
+                  </div>
+                </div>
+              <?php
+              }
+              ?>
             </div>
-          <?php
-          }
-          ?>
-        </div>
-        <p class="contenuHistoire"><?= $ligne['synopsis'] ?></p>
-    </article>
+            <p class="contenuHistoire"><?= $ligne['synopsis'] ?></p>
+        </article>
     <?php }
     } ?>
   </div>
-<?php include 'templatesHTML/footer.php'; ?>
+  <?php include 'templatesHTML/footer.php'; ?>
 
-<!-- jQuery -->
-<script src="../lib/jquery/jquery.min.js"></script>
-<!-- JavaScript Boostrap plugin -->
-<script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+  <!-- jQuery -->
+  <script src="../lib/jquery/jquery.min.js"></script>
+  <!-- JavaScript Boostrap plugin -->
+  <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
--- php/jeu.php
+++ php/jeu.php
@@ -9,28 +9,69 @@ require_once("connect.php");
 <?php include 'templatesHTML/head.php'; ?>
 
 <body>
-    <?php include 'templatesHTML/navbar.php';
-    if ($BDD) {
-        $req = "SELECT * FROM page_hist";
-        $res = $BDD->query($req);
-        $ligne = $res->fetch();
-        for ($i = 1; $i <= 5; $i++) {
-            $para = "para_" . $i;
-            $image = "img_" . $i; ?>
-            <div>
-                <?= $ligne[$para]; ?>
-            </div>
-            <div>
-                <img src="../images/moi aussi je suis un test/<?= $ligne[$image]; ?>" alt="<?= $ligne[$image]; ?>" />
-            </div>
-        <?php } ?>
+  <?php include 'templatesHTML/navbar.php';
+
+  if ($BDD) {
+    if (isset($_POST['id'])) {
+      $_SESSION['notreId'] = $_POST['id'];
+    }
+    $pageHist;
+    if (isset($_POST['idPageCible'])) {
+      $pageHist = $_POST['idPageCible'];
+    } else {
+      $req3 = "SELECT choix_eff FROM hist_jouee WHERE id_hist = '{$_SESSION['notreId']}'";
+      $res3 = $BDD->prepare($req3);
+      $res3->execute();
+      $ligne3 = $res3->fetch();
+      if (isset($ligne3['choix_eff'])) {
+        $pageHist = $ligne3['choix_eff'];
+      } else {
+        $pageHist = '0';
+      }
+    }
+
+
+    $req = "SELECT * FROM page_hist WHERE id_page = '{$pageHist}' AND WHERE id_hist = '{$_SESSION['notreId']}'";
+    $res = $BDD->prepare($req);
+    $res->execute();
+    $ligne = $res->fetch();
+    $identPage = $ligne["id_page"];
+    for ($i = 1; $i <= 5; $i++) {
+      $para = "para_" . $i;
+      $image = "img_" . $i; ?>
+      <div>
+        <?= $ligne[$para]; ?>
+      </div>
+      <div>
+        <!--remodifier le nom de la source-->
+        <img src="../images/moi aussi je suis un test/<?= $ligne[$image]; ?>" alt="<?= $ligne[$image]; ?>" />
+      </div>
     <?php } ?>
 
-<?php include 'templatesHTML/footer.php'; ?>
+  <?php } ?>
 
-<!-- jQuery -->
-<script src="../lib/jquery/jquery.min.js"></script>
-<!-- JavaScript Boostrap plugin -->
-<script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+  <?php
+  if ($BDD) {
+    $req2 = "SELECT * FROM choix WHERE id_page = '{$identPage}'";
+    $res2 = $BDD->prepare($req2);
+    $res2->execute();
+    $ligne2 = $res2->fetch();
+    while ($ligne2 = $res2->fetch()) { ?>
+      <div>
+        <a href=<?= "jeu.php?idPageCible=" . $ligne2['id_page_cible']; ?> a> <?= $ligne2['id_page_cible']; ?> </a>
+
+      </div>
+    <?php } ?>
+  <?php
+  }
+  ?>
+
+  <?php include 'templatesHTML/footer.php'; ?>
+
+  <!-- jQuery -->
+  <script src="../lib/jquery/jquery.min.js"></script>
+  <!-- JavaScript Boostrap plugin -->
+  <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
 </body>
+
 </html>
