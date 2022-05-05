9f47bb4fa7e04401ce6dd22827c4d3b114d4e722
Melipatamaa <pyrollineisafirefox@gmail.com>
2022-05-05 15:03:50 +0200

    gestion des points de vie

--- php/jeu.php
+++ php/jeu.php
@@ -12,12 +12,20 @@ require_once("connect.php");
   <?php include 'templatesHTML/navbar.php';
 
   if ($BDD) {
+
     if (isset($_GET['id'])) {
       $_SESSION['notreId'] = $_GET['id'];
     }
+
+    $_SESSION['nbpv'] = 3;
     $pageHist;
     if (isset($_GET['idPageCible'])) {
       $pageHist = $_GET['idPageCible'];
+      $req4 = "SELECT * FROM choix WHERE id_hist = '{$_SESSION['notreId']}' AND id_page_cible = '{$pageHist}'";
+      $res4 = $BDD->prepare($req4);
+      $res4->execute();
+      $ligne4 = $res4->fetch();
+      $_SESSION['nbpv'] += $ligne4['nb_pdv_perdu'];
     } else {
       $req3 = "SELECT * FROM hist_jouee WHERE id_hist = '{$_SESSION['notreId']}'";
       $res3 = $BDD->prepare($req3);
@@ -25,8 +33,10 @@ require_once("connect.php");
       $ligne3 = $res3->fetch();
       if (isset($ligne3['choix_eff'])) {
         $pageHist = $ligne3['choix_eff'];
+        $_SESSION['nbpv'] += $ligne3['nb_pts_vie'];
       } else {
         $pageHist = '0';
+        $_SESSION['nbpv'] = 3;
       }
     }
 
@@ -50,6 +60,15 @@ require_once("connect.php");
 
   <?php } ?>
 
+  <div>
+    <?php
+    if ($_SESSION['nbpv'] == 0) { ?>
+      <?= "Vous avez perdu."; ?>
+    <?php
+    }
+    ?>
+  </div>
+
   <?php
 
   if ($BDD) {
@@ -61,10 +80,11 @@ require_once("connect.php");
         <a href=<?= "jeu.php?idPageCible=" . $ligne2['id_page_cible']; ?> a> <?= $ligne2['id_page_cible']; ?> </a>
 
       </div>
-    <?php } ?>
-  <?php
-  }
-  ?>
+      <div>
+        <?php echo $_SESSION['nbpv']; ?>
+      </div>
+  <?php }
+  } ?>
 
   <?php include 'templatesHTML/footer.php'; ?>
 
