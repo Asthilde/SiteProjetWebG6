95295f33c6386d0d6e7e2f6883c68fca22177a9a
Melipatamaa <pyrollineisafirefox@gmail.com>
2022-05-06 11:50:47 +0200

    bootstrap ??

--- index.php
+++ index.php
@@ -139,20 +139,21 @@ require_once 'php/connect.php' ?>
         }
         if (!in_array($ligne['id_hist'], $tabHistJouee)) {
       ?>
-          <article>
+          <div class='container'>
             <!--Il faut vérifier ici si l'utilisateur est connecté -->
-            <div class="row align-items-end">
-              <h3 class="col"><?php echo $ligne['nom_hist']; ?></h3>
-              <div class="col" id="nomCreateur"> une histoire créée par <?= $maLigne['pseudo']; ?> </div>
-              <div class="contenuHistoire"><?= $ligne['synopsis'] ?></div>
-              <div class="form-group">
-                <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-                  <a class="btn btn-default btn-success" href="<?= $demarrage ?>"> Démarrer </a>
-                </div>
+            <div class="align-items-end">
+              <h3><?php echo $ligne['nom_hist']; ?></h3>
+              <div id="nomCreateur"> une histoire créée par <?= $maLigne['pseudo']; ?> </div>
+            </div>
+            <div class="row">
+              <div class="col-sm"><?= $ligne['synopsis'] ?></div>
+
+              <div class="col-sm form-group">
+                <a class="btn btn-default btn-success" href="<?= $demarrage ?>"> Démarrer </a>
                 <?php
                 if (isset($_SESSION["admin"]) &&  $_SESSION["admin"] == 1) {
                 ?>
-                  <div class="col" id="imgModifSuppr">
+                  <div id="imgModifSuppr">
                     <a class="bi bi-pencil-square" href="php/modifier.php?id=<?= $ligne['id_hist'] ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
@@ -160,7 +161,7 @@ require_once 'php/connect.php' ?>
                     <a class="bi bi-trash3-fill" href="php/supprimer.php?id=<?= $ligne['id_hist'] ?>&nomHist=<?= $ligne['nom_hist'] ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                       </svg></a>
-                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                    <div>
                       <a class="btn btn-default btn-success" href="php/histoires.php?id=<?= $ligne['id_hist'] ?>"> Plus d'infos </a>
                     </div>
                   </div>
@@ -168,18 +169,17 @@ require_once 'php/connect.php' ?>
                 }
                 ?>
               </div>
-
-          </article>
-    <?php }
+            </div>
+      <?php }
       }
     } ?>
-  </div>
-  <?php include 'php/templatesHTML/footer.php'; ?>
+          </div>
+          <?php include 'php/templatesHTML/footer.php'; ?>
 
-  <!-- jQuery -->
-  <script src="lib/jquery/jquery.min.js"></script>
-  <!-- JavaScript Boostrap plugin -->
-  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+          <!-- jQuery -->
+          <script src="lib/jquery/jquery.min.js"></script>
+          <!-- JavaScript Boostrap plugin -->
+          <script src="lib/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
