6c19f0ac4941564bd51fe9a8a549353f62832f06
Asthilde <noemie.pellegrin@live.fr>
2022-05-06 09:42:16 +0200

    Deplacement de l index

--- php/deconnexion.php
+++ php/deconnexion.php
@@ -1,5 +1,6 @@
 <?php session_start();
 session_unset();
 session_destroy();
-header('Location: index.php') ;
+$urlAccueil = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'],'php/deconnexion')).'index.php';
+header("Location:{$urlAccueil}");
 ?>
