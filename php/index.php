8e13f4d776bdf43831ad57d532ac27f047e5f800
Melipatamaa <pyrollineisafirefox@gmail.com>
2022-05-08 11:07:42 +0200

    la révolution : changement de librairie bootstrap + changements de mise en page, les flex marchent enfin

---
+++ php/index.php
@@ -0,0 +1,144 @@
+<?php
+session_start();
+require_once 'connect.php' ?>
+<!doctype html>
+<html>
+
+<?php include 'templatesHTML/head.php'; ?>
+
+<body>
+  <?php include 'templatesHTML/navbar.php'; ?>
+  <div class="container">
+    <?php
+    if ($BDD) {
+      $tabHistJouee = array();
+      $demarrage = "php/connexion.php";
+      if (isset($_SESSION['id_user'])) {
+        $req = "SELECT * FROM hist_jouee WHERE id_user = '{$_SESSION['id_user']}'";
+        $res = $BDD->prepare($req);
+        $res->execute();
+        $i = 0;
+        while ($ligne = $res->fetch()) {
+          if ($i == 0) { ?>
+            <div class="row align-items-end">
+              <h2 class="col p-5">Histoires en cours</h2>
+            </div>
+          <?php
+            $i++;
+          }
+          $demarrage = "php/jeu.php?id={$ligne['id_hist']}&pageDebut={$ligne['choix_eff']}&nbPdv={$ligne['nb_pts_vie']}";
+          array_push($tabHistJouee, $ligne['id_hist']);
+          $idHist = (int) $ligne['id_hist'];
+          $req2 = "SELECT * FROM histoire WHERE id_hist = {$idHist}";
+          $res2 = $BDD->query($req2);
+          $ligne2 = $res2->fetch();
+          $idCrea = (int) $ligne2['id_createur'];
+          $req3 = "SELECT * FROM user WHERE id_user = {$idCrea}";
+          $res3 = $BDD->query($req3);
+          $maLigne = $res3->fetch(); ?>
+          <article>
+
+            <div class="p-2 form-group">
+              <a class="btn btn-default btn-success" href="<?= $demarrage ?>"> Démarrer </a>
+              <div class="row align-items-end">
+                <h3><?php echo $ligne2['nom_hist']; ?></h3>
+                <div id="nomCreateur">Une histoire créée par <?= $maLigne['pseudo']; ?> </div>
+              </div>
+              <div class="d-flex flex-row justify-content-center">
+                <div class="contenuHistoire"><?= $ligne2['synopsis'] ?></div>
+                <div class="form-group">
+                  <div>
+                    <a class="btn btn-default btn-success" href="<?= $demarrage ?>">Continuer</a>
+                  </div>
+                </div>
+                <?php
+                if (isset($_SESSION["admin"]) &&  $_SESSION["admin"] == 1) {
+                ?>
+                  <div class="col" id="imgModifSuppr">
+                    <a class="bi bi-pencil-square" href="php/modifier.php?id=<?= $ligne2['id_hist'] ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                      </svg></a>
+                    <a class="bi bi-trash3-fill" href="php/supprimer.php?id=<?= $ligne2['id_hist'] ?>&nomHist=<?= $ligne2['nom_hist'] ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                      </svg></a>
+                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                      <a class="btn btn-default btn-success" href="php/histoires.php?id=<?= $ligne2['id_hist'] ?>"> Plus d'infos </a>
+                    </div>
+                  </div>
+                <?php
+                }
+                ?>
+              </div>
+            </div>
+          </article>
+      <?php }
+      } ?>
+      <div class="row align-items-end">
+        <?php if (isset($_SESSION['login'])) { ?>
+          <h2 class="col p-5">Autres histoires</h2>
+        <?php } else { ?>
+          <h2 class="col p-5 ">Histoires</h2>
+        <?php } ?>
+
+      </div>
+      <?php $req = "SELECT * FROM histoire ORDER BY nom_hist";
+      $res = $BDD->query($req);
+      while ($ligne = $res->fetch()) {
+        $req2 = "SELECT * FROM user WHERE id_user =" . $ligne['id_createur'];
+        $res2 = $BDD->query($req2);
+        $maLigne = $res2->fetch();
+        if (isset($_SESSION['login'])) {
+          $demarrage = "php/jeu.php?id={$ligne['id_hist']}";
+        }
+        if (!in_array($ligne['id_hist'], $tabHistJouee)) {
+      ?>
+          <div class='container'>
+            <!--Il faut vérifier ici si l'utilisateur est connecté -->
+            <div class="align-items-end">
+              <h3><?php echo $ligne['nom_hist']; ?></h3>
+              <div id="nomCreateur"> une histoire créée par <?= $maLigne['pseudo']; ?> </div>
+            </div>
+            <div class="d-flex flex-row justify-content-center">
+              <div class="p-2"><?= $ligne['synopsis'] ?></div>
+
+              <div class="p-2 form-group">
+                <a class="btn btn-default btn-success" href="<?= $demarrage ?>"> Démarrer </a>
+                <?php
+                if (isset($_SESSION["admin"]) &&  $_SESSION["admin"] == 1) {
+                ?>
+                  <div id="imgModifSuppr">
+                    <a class="bi bi-pencil-square" href="php/modifier.php?id=<?= $ligne['id_hist'] ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                      </svg></a>
+                    <a class="bi bi-trash3-fill" href="php/supprimer.php?id=<?= $ligne['id_hist'] ?>&nomHist=<?= $ligne['nom_hist'] ?>"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                      </svg></a>
+                    <div>
+                      <a class="btn btn-default btn-success" href="php/histoires.php?id=<?= $ligne['id_hist'] ?>"> Plus d'infos </a>
+                    </div>
+                  </div>
+                <?php
+                }
+                ?>
+              </div>
+            </div>
+
+
+
+
+      <?php }
+      }
+    } ?>
+          </div>
+  </div>
+  <?php include 'templatesHTML/footer.php'; ?>
+
+  <!-- jQuery -->
+  <script src="../lib/jquery/jquery.min.js"></script>
+  <!-- JavaScript Boostrap plugin -->
+  <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- php/templatesHTML/navbar.php
+++ php/templatesHTML/navbar.php
@@ -1,39 +1,33 @@
-<nav class="navbar navbar-fixed-top" role="navigation">
+<nav class="navbar  navbar-fixed-top" role="navigation">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-target">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="../index.php"><span class="glyphicon glyphicon-book"></span> Une histoire dont vous êtes le héros</a>
+      <a class="navbar-brand" href="../index.php"><img src="../images/bootstrapNEPASTOUCHER/book-half.svg" alt="petit livre"></img> Une histoire dont vous êtes le héros</a>
     </div>
-    <div class="collapse navbar-collapse" id="navbar-collapse-target">
+    <div>
       <?php if (isset($_SESSION['admin']) &&  $_SESSION['admin'] == 1) { ?>
-        <ul class="nav navbar-nav">
-          <li><a href="ajout_hist.php">Ajouter une histoire</a></li>
-        </ul>
-      <?php } ?>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <span class="glyphicon glyphicon-user"></span>
-            <?php if (isset($_SESSION['login'])) {
-              echo "Bienvenue, " . $_SESSION['login'];
-            } else {
-              echo "Non connecté";
-            } ?> <b class="caret"></b>
-          </a>
-          <ul class="dropdown-menu">
-            <?php if (isset($_SESSION['login'])) { ?>
-              <li><a href="deconnexion.php">Se déconnecter</a></li>
-            <?php } else { ?>
-              <li><a href="connexion.php">Se connecter</a></li>
-              <li><a href="creation_compte.php">Se créer un compte</a></li>
-            <?php } ?>
-          </ul>
-        </li>
-      </ul>
+        <a href="php/ajout_hist.php">Ajouter une histoire</a>
     </div>
+
+  <?php } ?>
+  <div class="dropdown">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <?php if (isset($_SESSION['login'])) {
+        echo "Bienvenue, " . $_SESSION['login'];
+      } else {
+        echo "Non connecté";
+      } ?> </a>
+    </a>
+    <div class="dropdown-menu">
+      <?php if (isset($_SESSION['login'])) { ?>
+        <a class="dropdown-item" href="../php/deconnexion.php">Se déconnecter</a>
+      <?php } else { ?>
+        <a class="dropdown-item" href="../php/connexion.php">Se connecter</a>
+        <a class="dropdown-item" href="../php/creation_compte.php">Se créer un compte</a>
+      <?php } ?>
+    </div>
+  </div>
+  </div>
+
+
   </div>
 </nav>
